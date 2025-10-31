13d0ff1a65d8551b0194dd7978a188bcb5aef6c3
Homura-san <erickbispo@ufba.br>
2025-10-31 20:52:07 -0300

    Usando componente

---
+++ fundamentos_nextjs/src/pages/exemplo.jsx
@@ -0,0 +1,15 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Exemplo() {
+    return (
+        <>
+            <Cabecalho />
+            <Cabecalho />
+        </>
+    )
+}
+
+/* 
+JSX fragment  <> </>  
+elementos dentro dela irão ficar numa div com id __next
+*/
