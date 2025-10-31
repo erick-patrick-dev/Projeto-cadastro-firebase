630a02faa02ce994027750e6bcb653d09a1ac5a8
Homura-san <erickbispo@ufba.br>
2025-10-30 23:22:18 -0300

    Javascript / JSX

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,5 +2,10 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 export default function Inicio() {
-  return "inicio"
+  return (
+    <div>
+      <h1>Fundamentos de Next.js & React</h1>
+      <h2>Estudando o framework</h2>
+    </div>
+  )
 }
--- src/pages/teste.jsx
+++
@@ -1,8 +0,0 @@
-// Qualquer arquivo dentro da pasta pages é tratado
-// como uma rota pelo Next.js
-
-export default function Teste() {
-    return "teste"
-}
-
-/* A estrutura de pastas irá definir as rotas */
