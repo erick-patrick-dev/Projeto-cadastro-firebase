8f7e05e9003b4079f8fc07f1ec9a542d8638fc94
Homura-san <erickbispo@ufba.br>
2025-11-06 22:39:26 -0300

    Integração com API

---
+++ fundamentos_nextjs/src/pages/integracao_1.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import Layout from "../components/Layout"
+
+export default function Integracao() {
+
+    const [codigo, setCodigo] = useState(1)
+    const [cliente, setCliente] = useState({})
+
+    async function obterCliente() {
+        const resp = await fetch(`http://localhost:3000/api/clientes/${codigo}`)
+        const dados = await resp.json()
+        setCliente(dados)
+
+        // Outra forma de fazer a requisição
+        // fetch(`http://localhost:3000/api/clientes/${codigo}`)
+        //     .then(res => res.json())
+        //     .then(dados => setCliente(dados))
+    }
+
+    return (
+        <Layout titulo="Integração com API #01">
+            <div>
+                <input type="number" value={codigo} onChange={e => setCodigo(e.target.value)} />
+                <button onClick={obterCliente}>Obter dados</button>
+            </div>
+            <ul>
+                <li>Código: {cliente.id} </li>
+                <li>Nome: {cliente.nome} </li>
+                <li>Email: {cliente.email} </li>
+            </ul>
+        </Layout>
+    )
+}
