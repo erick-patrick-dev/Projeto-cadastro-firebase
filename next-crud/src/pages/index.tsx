07e67e0031f6fb4dfd6670f89b7c5e7c316614b4
Homura-san <erickbispo@ufba.br>
2025-11-11 22:35:39 -0300

    Componente Tabela #01

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+  
+  const clientes = [
+    new Cliente('Ana', 30, '1'),
+    new Cliente('Caio', 11, '7'),
+    new Cliente('Tereza', 63, '2'),
+    new Cliente('Rosa', 44, '8')
+  ]
+
   return (
     <div className={`
       flex h-screen justify-center items-center
@@ -8,7 +18,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Página Inicial">
-        <h1>Conteúdo da Página Inicial</h1>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   );
